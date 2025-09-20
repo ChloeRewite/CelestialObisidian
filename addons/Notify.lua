03d5558935b4579def6b30f61a56c38397402b3e
ChloeBaruDewasa <dimasadipurwanto@gmail.com>
2025-09-20 12:21:22 +0700

    Update Notify.lua

--- addons/Notify.lua
+++ addons/Notify.lua
@@ -8,39 +8,6 @@ local function Chloe(desc, time, sound)
         Time = time or 3,
         SoundId = sound,
     })
-
-    task.spawn(function()
-        local title = note and note.Frame and note.Frame.Title
-        if title then
-            -- MATIIN RichText biar ga auto split
-            title.RichText = false
-
-            -- font dibold manual
-            title.Font = Enum.Font.GothamBold
-
-            -- biar aman ga kebelah 2
-            title.TextWrapped = false
-            title.TextXAlignment = Enum.TextXAlignment.Left
-            title.TextTruncate = Enum.TextTruncate.AtEnd
-
-            -- kalau width terlalu kecil, paksa perbesar sedikit
-            if title.TextBounds.X > title.AbsoluteSize.X then
-                title.Size = UDim2.new(1, 50, title.Size.Y.Scale, title.Size.Y.Offset)
-            end
-
-            -- kasih gradient
-            if not title:FindFirstChild("UIGradient") then
-                local gradient = Instance.new("UIGradient")
-                gradient.Color = ColorSequence.new{
-                    ColorSequenceKeypoint.new(0, Color3.fromRGB(140, 82, 255)),
-                    ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 191, 255))
-                }
-                gradient.Rotation = 90
-                gradient.Parent = title
-            end
-        end
-    end)
-
     return note
 end
 
